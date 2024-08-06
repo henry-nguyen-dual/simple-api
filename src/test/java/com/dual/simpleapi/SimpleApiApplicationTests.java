acf0c08a10d9204f46f4f9c227a8c985921673fe
nguyenl6 <nguyenl6@risk.regn.net>
2024-08-06 10:52:18 +0700

    Init project

---
+++ src/test/java/com/dual/simpleapi/SimpleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dual.simpleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
