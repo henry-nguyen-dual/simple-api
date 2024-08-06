8c6a02808392b4e28e019399c840e2c48c5f2ed3
nguyenl6 <nguyenl6@risk.regn.net>
2024-08-06 11:16:19 +0700

    Add simple API

--- src/main/java/com/dual/simpleapi/SimpleApiApplication.java
+++ src/main/java/com/dual/simpleapi/SimpleApiApplication.java
@@ -2,12 +2,40 @@ package com.dual.simpleapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
+@ControllerAdvice
 public class SimpleApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleApiApplication.class, args);
     }
 
+    @PostMapping(value = "/api/test", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> test(@RequestBody Map<String, Object> body) {
+        if (body == null) {
+            throw new IllegalArgumentException("Body is null");
+        }
+
+        Object age = body.get("age");
+        if (age == null) {
+            throw new IllegalArgumentException("Age is null");
+        }
+
+        if (!(age instanceof Integer)) {
+            throw new IllegalArgumentException("Age is not an integer");
+        }
+
+        return Map.of("isAdult", (int) age >= 18);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public Map<String, Object> handleIllegalArgumentException(IllegalArgumentException e) {
+        return Map.of("error", e.getMessage());
+    }
 }
